f6c060e95a0f0136acfb9974ac0efdbf090d1780
swen2014 <steven2hddx@gmail.com>
2015-01-27 19:42:29 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,22 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by steven on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+@end
