98db76b30397af0a3efb1b06e891b6a3c9d288e1
swen2014 <steven2hddx@gmail.com>
2015-01-31 22:02:47 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -20,31 +20,33 @@
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
     
-    CCNode *_currentPenguin;
-    //Penguin *_currentPenguin;
+    //CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
+    
+    CCAction *_followPenguin;
 }
 
+static const float MIN_SPEED = 5.f;
 
 #pragma mark - Init
 
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
+    // nothing shall collide with our invisible nodes
+    _mouseJointNode.physicsBody.collisionMask = @[];
+    _pullbackNode.physicsBody.collisionMask = @[];
+    
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
     //
     //CCScene *level = [CCBReader loadAsScene:@"Level1"];
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
-
     [_levelNode addChild:level];
     //
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
     //
-    // nothing shall collide with our invisible nodes
-    _mouseJointNode.physicsBody.collisionMask = @[];
-    _pullbackNode.physicsBody.collisionMask = @[];
-    
     _physicsNode.collisionDelegate = self;
 }
 
@@ -73,10 +75,16 @@
         
         // after snapping rotation is fine
         _currentPenguin.physicsBody.allowsRotation = TRUE;
+//         _currentPenguin.launched = YES;
+        _currentPenguin.launched = TRUE;
         
         // follow the flying penguin
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
+        _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:_followPenguin];
+        
+//        // follow the flying penguin
+//        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+//        [_contentNode runAction:follow];
     }
 }
 
@@ -97,6 +105,14 @@
     [seal removeFromParent];
 }
 
+- (void)nextAttempt {
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+    
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+    [_contentNode runAction:actionMoveTo];
+}
+
 
 #pragma mark - Touch Handling
 
@@ -115,7 +131,8 @@
         //anchor B 34,138   restLength:0.f stiffness:3000.f damping:150.f
         
         // create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
+        _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
+        //        _currentPenguin = [CCBReader load:@"Penguin"];
         // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
         CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
         // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -192,5 +209,30 @@
 
 #pragma mark - Update
 
+- (void)update:(CCTime)delta
+{
+    if (_currentPenguin.launched) {
+
+    // if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMin = _currentPenguin.boundingBox.origin.x;
+    
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+        [self nextAttempt];
+        return;
+    }
+    }
+}
 
 @end
