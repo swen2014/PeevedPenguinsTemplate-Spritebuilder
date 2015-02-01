89358e138238c64b498690b5ffabd31c515db0ba
swen2014 <steven2hddx@gmail.com>
2015-01-31 21:17:52 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -33,8 +33,8 @@
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
     //
-    CCScene *level = [CCBReader loadAsScene:@"Level1"];
-    //CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    //CCScene *level = [CCBReader loadAsScene:@"Level1"];
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
 
     [_levelNode addChild:level];
     //
