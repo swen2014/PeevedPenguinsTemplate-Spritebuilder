fd122cd8c16a018b991d3b25b60690d9da4df13d
swen2014 <steven2hddx@gmail.com>
2015-02-03 15:44:20 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -89,17 +89,19 @@ static const float MIN_SPEED = 5.f;
 }
 
 - (void)sealRemoved:(CCNode *)seal {
-    [seal removeFromParent];
+    //[seal removeFromParent];
     
     //
     // load particle effect
     CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
-    // make the particle effect clean itself up, once it is completed
-    explosion.autoRemoveOnFinish = TRUE;
+
     // place the particle effect on the seals position
     explosion.position = seal.position;
     // add the particle effect to the same node the seal is on
     [seal.parent addChild:explosion];
+    // make the particle effect clean itself up, once it is completed
+    explosion.autoRemoveOnFinish = TRUE;
+
     
     // finally, remove the destroyed seal
     [seal removeFromParent];
