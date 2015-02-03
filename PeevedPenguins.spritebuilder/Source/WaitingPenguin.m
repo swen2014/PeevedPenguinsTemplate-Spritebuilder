e3d38e5f032d073d69ac6303ec725c2141df8f9d
swen2014 <steven2hddx@gmail.com>
2015-02-03 15:57:06 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -10,19 +10,19 @@
 
 @implementation WaitingPenguin
 
-//- (void)didLoadFromCCB
-//{
-//    // generate a random number between 0.0 and 2.0
-//    float delay = (arc4random() % 2000) / 1000.f;
-//    // call method to start animation after random delay
-//    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
-//}
-//
-//- (void)startBlinkAndJump
-//{
-//    // the animation manager of each node is stored in the 'animationManager' property
-//    CCAnimationManager* animationManager = self.animationManager;
-//    // timelines can be referenced and run by name
-//    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
-//}
+- (void)didLoadFromCCB
+{
+    // generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    // call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+}
+
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
 @end
